83cc8312e96be680d0c64504aaa8606a6c604184
narfinlover <narfinlover@gmail.com>
2026-07-07 16:10:38 +0400

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabaseUrl = "https://yfmvqdicqirsjhdxrtjf.supabase.co";
+const supabaseKey = "sb_publishable_HOH-C_Z1is6gcRBIoLfvMg_7qGWHeuH";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 document.getElementById("createGroup").addEventListener("click", () => {
     alert("Create Group feature coming soon!");
 });
@@ -6,15 +13,36 @@ document.getElementById("joinGroup").addEventListener("click", () => {
     alert("Join Group feature coming soon!");
 });
 
-document.getElementById("sendMessage").addEventListener("click", () => {
+document.getElementById("sendMessage").addEventListener("click", async () => {
     const input = document.getElementById("messageInput");
-    const messages = document.getElementById("messages");
 
     if (input.value.trim() !== "") {
-        const msg = document.createElement("p");
-        msg.textContent = "You: " + input.value;
-        messages.appendChild(msg);
+
+        await supabase
+            .from("messages")
+            .insert([
+                { message: input.value }
+            ]);
+
         input.value = "";
-        messages.scrollTop = messages.scrollHeight;
+        loadMessages();
     }
 });
+
+async function loadMessages() {
+    const messages = document.getElementById("messages");
+    messages.innerHTML = "";
+
+    const { data } = await supabase
+        .from("messages")
+        .select("*")
+        .order("created_at", { ascending: true });
+
+    data.forEach(msg => {
+        const p = document.createElement("p");
+        p.textContent = msg.message;
+        messages.appendChild(p);
+    });
+}
+
+loadMessages();
